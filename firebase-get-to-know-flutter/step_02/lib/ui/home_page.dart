a55abf131c30d1fa597f17905defbb766bae8abd
Kweayon  Clark <Kweayon@gmail.com>
2023-03-04 13:24:41 -0600

    Refactor

--- firebase-get-to-know-flutter/step_02/lib/ui/home_page.dart
+++ firebase-get-to-know-flutter/step_02/lib/ui/home_page.dart
@@ -1,6 +1,8 @@
 // Copyright 2022 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// ignore_for_file: directives_ordering
+
 import '../widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' // new
@@ -10,6 +12,7 @@ import 'package:firebase_auth/firebase_auth.dart' // new
 import 'package:provider/provider.dart';
 import '../state/app_state.dart';
 import 'authentication.dart';
+import 'guest_book.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,7 +23,16 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Firebase Meetup'),
       ),
-      body: ListView(
+      body: const _CustomListView(),
+    );
+  }
+}
+
+class _CustomListView extends StatelessWidget {
+  const _CustomListView();
+
+  @override
+  Widget build(BuildContext context) => ListView(
         children: <Widget>[
           Image.asset('assets/codelab.png'),
           const SizedBox(height: 8),
@@ -33,19 +45,28 @@ class HomePage extends StatelessWidget {
                   FirebaseAuth.instance.signOut();
                 }),
           ),
-          const Divider(
-            height: 8,
-            thickness: 1,
-            indent: 8,
-            endIndent: 8,
-            color: Colors.grey,
-          ),
+          const _CustomDivider(),
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
+          const Header('Discussion'),
+          const GuestBook(addMessage: _handleMessageCallback)
         ],
-      ),
-    );
-  }
+      );
+}
+
+void _handleMessageCallback(String message) => print(message);
+
+class _CustomDivider extends StatelessWidget {
+  const _CustomDivider();
+
+  @override
+  Widget build(BuildContext context) => const Divider(
+        height: 8,
+        thickness: 1,
+        indent: 8,
+        endIndent: 8,
+        color: Colors.grey,
+      );
 }
--- firebase-get-to-know-flutter/step_02/test1.dart
+++ firebase-get-to-know-flutter/step_02/test1.dart
@@ -1,9 +1,7 @@
-void main() {
-  num times2(num x) => x * 2;
-
-  final myMap = {times2: times2};
-
-  final Map<String, num Function(num)> map1 = {'func1': times2};
-  final _times2 = map1['func1'] as num Function(num);
-  print(_times2(3));
+int main() {
+  final int num1;
+  num1 = 43;
+  print(num1);
+  print('Free, ' r'$400.00');
+  return 0;
 }
