03165586d83c9b427caddcc1aae84479bc0687a8
Kweayon  Clark <Kweayon@gmail.com>
2023-03-03 21:07:57 -0600

    Update

--- firebase-get-to-know-flutter/step_02/lib/main.dart
+++ firebase-get-to-know-flutter/step_02/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'home_page.dart';
+import 'app.dart';
 import 'state/app_state.dart';
 
 void main() async {
@@ -15,107 +12,3 @@ void main() async {
     builder: (context, child) => const App(),
   ));
 }
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Firebase Meetup',
-      theme: _theme(context),
-      routerConfig: _router,
-    );
-  }
-}
-
-ThemeData _theme(BuildContext context) => ThemeData(
-      buttonTheme: Theme.of(context).buttonTheme.copyWith(
-            highlightColor: Colors.deepPurple,
-          ),
-      primarySwatch: Colors.deepPurple,
-      textTheme: GoogleFonts.robotoTextTheme(
-        Theme.of(context).textTheme,
-      ),
-      visualDensity: VisualDensity.adaptivePlatformDensity,
-      useMaterial3: true,
-    );
-
-final _router = GoRouter(routes: <RouteBase>[
-  GoRoute(path: '/', builder: _homePageCallback, routes: [
-    GoRoute(path: 'sign-in', builder: _signInScreenCallback, routes: [
-      GoRoute(path: 'forgot-password', builder: _forgotPasswordCallback)
-    ]),
-    GoRoute(path: 'profile', builder: _profileCallback)
-  ]),
-]);
-
-Widget _homePageCallback(BuildContext context, GoRouterState? state) =>
-    const HomePage();
-
-Widget _signInScreenCallback(BuildContext context, GoRouterState state) =>
-// TODO: Wrap the Firebase UI Components with a Scafforld / AppBar for route poping
-    customScaffold(
-        child: SignInScreen(
-      actions: [
-        ForgotPasswordAction((context, email) {
-          final uri = Uri(
-            path: '/sign-in/forgot-password',
-            queryParameters: <String, String?>{
-              'email': email,
-            },
-          );
-          context.push(uri.toString());
-        }),
-        AuthStateChangeAction(_authStateCallback)
-      ],
-    ));
-
-Widget _forgotPasswordCallback(BuildContext context, GoRouterState state) {
-  final arugments = state.queryParams;
-  return customScaffold(
-      child: ForgotPasswordScreen(
-    email: arugments['email'],
-    headerMaxExtent: 200,
-  ));
-}
-
-Widget _profileCallback(BuildContext context, GoRouterState state) =>
-    ProfileScreen(
-      providers: const [],
-      actions: [
-        SignedOutAction((context) {
-          context.pushReplacement('/');
-        })
-      ],
-    );
-
-void _authStateCallback(BuildContext context, AuthState state) {
-  if (state is SignedIn || state is UserCreated) {
-    var user = (state is SignedIn)
-        ? state.user
-        : (state as UserCreated).credential.user;
-    if (user == null) {
-      return;
-    }
-    if (state is UserCreated) {
-      user.updateDisplayName(user.email!.split('@')[0]);
-    }
-    if (!user.emailVerified) {
-      user.sendEmailVerification();
-      // Declare snack bar
-      const snackBar = SnackBar(
-          content:
-              Text('Please check your email to verify your email address.'));
-      // Call snackbar
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
-    context.pushReplacement('/');
-  }
-}
-
-Widget customScaffold({required Widget child}) => Scaffold(
-    appBar: AppBar(
-      title: const Text('Firebase Demo'),
-    ),
-    body: child);
--- firebase-get-to-know-flutter/step_02/test1.dart
+++ firebase-get-to-know-flutter/step_02/test1.dart
@@ -1,9 +1,9 @@
 void main() {
-  num times2(num x) {
-    final num result = x * 2;
-    return result;
-  }
+  num times2(num x) => x * 2;
 
-  final map1 = {'func1': times2};
-  print(map1['func1']!(3));
+  final myMap = {times2: times2};
+
+  final Map<String, num Function(num)> map1 = {'func1': times2};
+  final _times2 = map1['func1'] as num Function(num);
+  print(_times2(3));
 }
