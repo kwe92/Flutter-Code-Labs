1840584dec73f8a00ac34d704e6b356fe1d58d25
Kweayon  Clark <Kweayon@gmail.com>
2023-03-06 22:48:25 -0600

    Update

--- firebase-get-to-know-flutter/step_02/lib/ui/guest_book.dart
+++ firebase-get-to-know-flutter/step_02/lib/ui/guest_book.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../constant/models/guest_book_message.dart';
 import '../widgets/widgets.dart';
 
 class GuestBook extends StatefulWidget {
   final FutureOr<void> Function(String message) addMessage;
+  final List<GuestBookMessage> messages;
 
-  const GuestBook({required this.addMessage, super.key});
+  const GuestBook(
+      {required this.addMessage, required this.messages, super.key});
 
   @override
   State<GuestBook> createState() => _GuestBookState();
@@ -16,13 +22,36 @@ class _GuestBookState extends State<GuestBook> {
   final _controller = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: _Form(
-          formKey: _formKey,
-          controller: _controller,
-          addMessage: widget.addMessage),
+  Widget build(BuildContext mainContext) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: _Form(
+              formKey: _formKey,
+              controller: _controller,
+              addMessage: widget.addMessage),
+        ),
+        const SizedBox(height: 8),
+        for (var message in widget.messages)
+          GestureDetector(
+            child: Paragraph('${message.name}: ${message.message}'),
+            onLongPress: () {
+              final FirebaseAuth auth = FirebaseAuth.instance;
+              final User? user = auth.currentUser;
+              final String? uid = user?.uid;
+              if (uid == message.id) {
+                print('the ids are the SAME!');
+              }
+              print('CURRENT USER: $uid');
+              print('CURRENT USER: ${user?.displayName}');
+
+              _bottomSheet(mainContext);
+            },
+          ),
+        const SizedBox(height: 8),
+      ],
     );
   }
 }
@@ -82,3 +111,37 @@ Widget _button(
             controller.clear();
           }
         });
+
+PersistentBottomSheetController _bottomSheet(BuildContext context) =>
+    showBottomSheet(
+      context: context,
+      builder: (context) => SizedBox(
+          height: 150,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(top: 0.0),
+                  child: Text('Are you sure you want to delete the message?'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    context.pop();
+                  },
+                  child: Text(
+                    'DELETE',
+                    style: TextStyle(color: Colors.red.shade500),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    context.pop();
+                  },
+                  child: const Text('Go Back'),
+                )
+              ],
+            ),
+          )),
+    );
