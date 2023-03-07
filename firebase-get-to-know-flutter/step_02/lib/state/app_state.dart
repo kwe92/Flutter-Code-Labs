f6d9c3f5441a2d0192b8dee2c80b8bcbfafb50a2
Kweayon  Clark <Kweayon@gmail.com>
2023-03-06 22:48:13 -0600

    Update

--- firebase-get-to-know-flutter/step_02/lib/state/app_state.dart
+++ firebase-get-to-know-flutter/step_02/lib/state/app_state.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_final_fields
+
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
@@ -5,12 +7,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import '../constant/models/guest_book_message.dart';
 import '../firebase_options.dart';
+import '../ui/guest_book.dart';
 
 class ApplicationState extends ChangeNotifier {
   bool _loggedIn = false;
   bool get loggedIn => _loggedIn;
   StreamSubscription<QuerySnapshot>? _guestBookSubscription;
+
+  List<GuestBookMessage> _guestBookMessages = [];
+
+  List<GuestBookMessage> get guestBookMessages => _guestBookMessages;
 
   ApplicationState() {
     _init();
@@ -22,19 +30,63 @@ class ApplicationState extends ChangeNotifier {
 
     FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
 
-    FirebaseAuth.instance.userChanges().listen((user) {
-      user != null ? _loggedIn = true : _loggedIn = false;
+    FirebaseAuth.instance.userChanges().listen(
+      (user) {
+        user != null ? _loggedIn = true : _loggedIn = false;
 
-      notifyListeners();
-    });
+        if (user != null) {
+          _loggedIn = true;
+          _guestBookSubscription = FirebaseFirestore.instance
+              .collection('guestbook')
+              .orderBy('timestamp', descending: true)
+              .snapshots()
+              .listen((snapshot) {
+            _guestBookMessages = [];
+            for (final document in snapshot.docs) {
+              _guestBookMessages.add(
+                GuestBookMessage(
+                    id: document.data()['userid'] as String,
+                    name: document.data()['name'] as String,
+                    message: document.data()['text'] as String),
+              );
+            }
+            notifyListeners();
+          });
+        } else {
+          _loggedIn = false;
+          _guestBookMessages = [];
+          _guestBookSubscription?.cancel();
+        }
+
+        notifyListeners();
+      },
+    );
   }
 
   Future? addMessageToGuestBook(String message) => !_loggedIn
       ? throw Exception('Must be logged in.')
-      : FirebaseFirestore.instance.collection('guestbook').add(<String, Object>{
-          'text': message,
-          'timestamp': DateTime.now().millisecondsSinceEpoch,
-          'name': FirebaseAuth.instance.currentUser!.displayName as Object,
-          'userid': FirebaseAuth.instance.currentUser!.displayName as Object,
-        });
+      : FirebaseFirestore.instance.collection('guestbook').add(
+          <String, Object>{
+            'text': message,
+            'timestamp': DateTime.now().millisecondsSinceEpoch,
+            'name': FirebaseAuth.instance.currentUser!.displayName as Object,
+            'userid': FirebaseAuth.instance.currentUser!.uid,
+          },
+        );
+
+  // TODO: Refactor?
+  // Future? addMessageToGuestBook(String message) {
+  //   if (!_loggedIn) {
+  //     throw Exception('Must be logged in.');
+  //   } else {
+  //     FirebaseFirestore.instance.collection('guestbook').add();
+  //   }
+  // }
 }
+
+// <String, Object>{
+//         'text': message,
+//         'timestamp': DateTime.now().millisecondsSinceEpoch,
+//         'name': FirebaseAuth.instance.currentUser!.displayName as Object,
+//         'userid': FirebaseAuth.instance.currentUser!.uid,
+//       }
