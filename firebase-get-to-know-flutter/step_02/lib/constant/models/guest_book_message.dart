d2340d63c1c63b60f8b35e2f7ea8336c70ce769f
Kweayon  Clark <Kweayon@gmail.com>
2023-03-07 15:55:54 -0600

    Update

--- firebase-get-to-know-flutter/step_02/lib/constant/models/guest_book_message.dart
+++ firebase-get-to-know-flutter/step_02/lib/constant/models/guest_book_message.dart
@@ -1,8 +1,31 @@
 class GuestBookMessage {
-  final String id;
+  final String userID;
   final String name;
   final String message;
+  final String textID;
 
   const GuestBookMessage(
-      {required this.id, required this.name, required this.message});
+      {required this.textID,
+      required this.userID,
+      required this.name,
+      required this.message});
+
+  factory GuestBookMessage.fromJSON(Map<String, Object> json) =>
+      GuestBookMessage(
+          textID: json['textID'] as String,
+          userID: json['userid'] as String,
+          name: json['name'] as String,
+          message: json['text'] as String);
+
+  Map<String, Object> toJSON() {
+    final int timestamp = DateTime.now().millisecondsSinceEpoch;
+
+    return <String, Object>{
+      'textID': textID,
+      'text': message,
+      'timestamp': timestamp,
+      'name': name,
+      'userid': userID,
+    };
+  }
 }
